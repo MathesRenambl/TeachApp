552b1a58cbef9190d2f4d7981d416d68de3cef23
MathesRenambl <matheswaran@renambl.com>
2025-08-14 14:50:58 +0530

    feat: created-TestCustomizer

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import ExamApp from './ExamApp';
+import TestCustomizer from './app/TestCustomizer';
 
 export default function App() {
   return (
-   <ExamApp/>
+
+   <TestCustomizer />
   );
 }
 
